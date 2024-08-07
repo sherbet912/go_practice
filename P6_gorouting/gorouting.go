5102778f612e40fac69ba0861521816af5967369
sherbet912 <chouchuanyi12@gmail.com>
2024-08-07 10:11:33 +0800

    feat: 🎸 gorouting base

--- P6_gorouting/gorouting.go
+++ P6_gorouting/gorouting.go
@@ -1,37 +1,16 @@
 package main
 
 import (
-	. "fmt"
-	"math/rand"
-	// "runtime"
+	"fmt"
 	"time"
 )
-
 func say(s string) {
 	for i := 0; i < 5; i++ {
-		// runtime.Gosched()
-		Println(s)
-		sleep_time := rand.Intn(5)
-		time.Sleep(time.Duration(sleep_time))
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
 	}
 }
-
-func fibonacci(n int, c chan int) {
-    x, y := 1, 1
-    for i := 0; i < n; i++ {
-        c <- x
-        x, y = y, x + y
-    }
-    close(c)
-}
-
 func main() {
-	// go say("helloooooo")
-	// say("world!!!!")
-
-	c := make(chan int, 10)
-    go fibonacci(cap(c), c)
-    for i := range c {
-        Println(i)
-    }
+	go say("world")
+	say("hello")
 }
