797c88703c0d10779bd743a313d8364f39968237
sherbet912 <chouchuanyi12@gmail.com>
2024-10-29 11:43:53 +0800

    feat: 🎸 p11, gin, group route

--- P11_gin/main.go
+++ P11_gin/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	router := gin.Default()
 	
+	// 基礎 routes
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Welcome!")
 	})
@@ -17,5 +18,23 @@ func main() {
 		"message": "pong",
 	  })
 	})
+
+	// group routes
+	// create a new group
+	v1 := router.Group("v1")
+	v2 := router.Group("v2")
+	// Define a route for the root URL 
+	v1.GET("/hello", hello_v1)		// 0.0.0.0:8080/v1/hello
+	v1.GET("/hello_v2", hello_v2)	// 0.0.0.0:8080/v1/hello_v2
+	v2.GET("/hello", hello_v2)		// 0.0.0.0:8080/v2/hello
+
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-  }
+}
+
+func hello_v1(c *gin.Context) {
+	c.String(http.StatusOK, "Hello v1.")
+}
+
+func hello_v2(c *gin.Context) {
+	c.String(http.StatusOK, "Hello v2.")
+}
